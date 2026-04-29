c6473ff2ad47bdd67e20c0f1dc0b1942e60385d1
raducksijaa <ronald.alan77@gmail.com>
2026-04-30 07:51:25 +0800

    Update nikki.sh

--- nikki/nikki.sh
+++ nikki/nikki.sh
@@ -1,32 +1,80 @@
 #!/bin/sh
 
-opkg() {
-opkg update >/dev/null 2>&1
+IPK_DIR="/tmp/nikki"
+
+LUCIPKG="https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/luci-app-nikki_1.26.0-r1_all.ipk"
+MIHOMO="https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/mihomo-meta_1.19.23_aarch64_cortex-a53.ipk"
+NIKKI="https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/nikki_2026.04.08-r1_aarch64_cortex-a53.ipk"
+YQ="https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/yq_4.52.2-r1_aarch64_cortex-a53.ipk"
+
+die() {
+	echo ""
+	echo "ERROR: $1"
+	echo ""
+	exit 1
 }
 
-nikki() {
-wget -O /tmp/nikki/a.ipk https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/luci-app-nikki_1.26.0-r1_all.ipk >/dev/null 2>&1
-wget -O /tmp/nikki/b.ipk https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/mihomo-meta_1.19.23_aarch64_cortex-a53.ipk >/dev/null 2>&1
-wget -O /tmp/nikki/c.ipk https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/nikki_2026.04.08-r1_aarch64_cortex-a53.ipk >/dev/null 2>&1
-wget -O /tmp/nikki/a.ipk https://raw.githubusercontent.com/Razifadm/radu/ipk/nikki/yq_4.52.2-r1_aarch64_cortex-a53.ipk >/dev/null 2>&1
+update_opkg() {
+	echo "[1/4] Updating opkg..."
+	command opkg update >/dev/null 2>&1 || die "opkg update failed"
+}
+
+prepare_dir() {
+	rm -rf "$IPK_DIR"
+	mkdir -p "$IPK_DIR"
+}
+
+download_file() {
+	OUT="$1"
+	URL="$2"
+
+	echo "Downloading: $OUT"
+	wget -q -O "$IPK_DIR/$OUT" "$URL" || die "Download failed: $OUT"
+
+	[ -s "$IPK_DIR/$OUT" ] || die "Downloaded file empty: $OUT"
+}
+
+download_nikki() {
+	echo "[3/4] Downloading Nikki packages..."
+
+	download_file "luci-app-nikki.ipk" "$LUCIPKG"
+	download_file "mihomo-meta.ipk" "$MIHOMO"
+	download_file "nikki.ipk" "$NIKKI"
+	download_file "yq.ipk" "$YQ"
+}
+
+install_nikki() {
+	echo "[4/4] Installing Nikki..."
+
+	command opkg install "$IPK_DIR"/*.ipk || die "Nikki installation failed"
+}
+
+cleanup() {
+	rm -rf "$IPK_DIR"
 }
 
 main() {
-opkg
-nikki
-opkg install /tmp/nikki/*.ipk
-sleep 1
-rm -rf /tmp/nikki
+	clear
+	echo "================================"
+	echo " Installing Nikki"
+	echo " Currently only RaduImmo.bin supported"
+	echo "================================"
+	echo ""
+
+	update_opkg
+	prepare_dir
+	download_nikki
+	install_nikki
+	cleanup
+
+	echo ""
+	echo "================================"
+	echo " Installation Done"
+	echo " Access Nikki at:"
+	echo " Services > Nikki"
+	echo "================================"
+	echo ""
 }
 
-clear
-echo "Installing Nikki"
-echo "Currently only RaduImmo.bin support this"
-main 
-echo ""
-echo "Installation Done"
-echo ""
-echo "access nikki at"
-echo "Services-nikki"
-
+main
 exit 0
